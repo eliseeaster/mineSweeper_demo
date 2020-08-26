29be46a4ab8393c80503ffd7d50bdc94b92c8339
Elise Easter <56083509+eliseeaster@users.noreply.github.com>
2020-08-26 07:30:28 +0200

    Initial commit

---
+++ src/no/kristiania/minesweeper/Minefieldtest.java
@@ -0,0 +1,15 @@
+package no.kristiania.minesweeper;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class Minefieldtest {
+    @Test
+    void itShowsEmptyMineField (){
+        assertArrayEquals(new String[] {"000", "000"}, displayMineField(new String [] {"...", ",,,"}));
+    }
+    private Object[] displayMineField(String[] strings) {
+        return new String[0];
+    }
+}
